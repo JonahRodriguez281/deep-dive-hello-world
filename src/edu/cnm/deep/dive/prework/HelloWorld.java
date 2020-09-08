1cb34a78f16f1d7cd4070b8d4176284f1754aca6
Jonah Rodriguez <jonahrodriguez95@gmail.com>
2020-09-08 11:08:50 -0600

    Reorganized project files.

--- src/edu/cnm/deep/dive/prework/HelloWorld.java
+++ src/edu/cnm/deep/dive/prework/HelloWorld.java
@@ -5,9 +5,11 @@ public class HelloWorld {
   public static void main(String[] args) {
     salute(salutation(args));
   }
+
   private static String salutation(String[] args) {
     return "Hello, " + ((args.length > 0) ? args[0] : "World");
   }
+
   private static void salute(String salutation) {
     System.out.println(salutation);
   }
